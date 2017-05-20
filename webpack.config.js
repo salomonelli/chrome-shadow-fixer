293e9f462bddea635ba41243a6125505470e60ad
salomonelli <sara.steiert@outlook.com>
2017-05-20 18:46:02 +0200

    ADD es next

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
 const path = require('path');
 
-module.exports = {
-  entry: ['./src/javascript.js'],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'javascript.js',
-    libraryTarget: 'var',
-    library: 'ChromeShadowFixer'
+module.exports = [
+  {
+    entry: ['./src/javascript.js'],
+    output: {
+      path: path.join(__dirname, 'dist/lib'),
+      filename: 'javascript.js',
+      libraryTarget: 'var',
+      library: 'ChromeShadowFixer'
+    },
+    module: {
+      loaders: [{
+        loader: 'babel-loader',
+        options: {
+          'presets': ['es2015']
+        }
+      }]
+    }
   },
-  module: {
-    loaders: [{
-      loader: 'babel-loader',
-      options: {
-        'presets': ['es2015']
-      }
-    }]
+  {
+    entry: ['./src/javascript.js'],
+    output: {
+      path: path.join(__dirname, 'dist/es'),
+      filename: 'javascript.js'
+    }
   }
-};
+];
